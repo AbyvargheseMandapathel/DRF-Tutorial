25c2dea09be4ce52bfeae801f7ac6f97cd68bf31
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-10-30 13:29:02 +0530

    updates

--- home/serializers.py
+++ home/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
-from .models import Person
+from .models import Person , Color
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = [
+            'color_name'
+        ]
+        
 
 class PeopleSerializer(serializers.ModelSerializer):
+    color_id = ColorSerializer()
     class Meta:
         model = Person
-        fields = [
-            'name',
-            'age'
-        ]
+        # fields = [
+        #     'name',
+        #     'age'
+        # ]
+        fields = "__all__"
+        
+    def validate(self, data):
+        if data['age'] < 18:
+            raise serializers.ValidationError ("Age should be greater than  18")
+        return data
